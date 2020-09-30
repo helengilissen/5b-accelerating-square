4d0d4c400e9efaf056a0a022421751f8163118b6
Helen Gilissen <55249087+helengilissen@users.noreply.github.com>
2020-09-30 10:45:32 +0200

    added 5b accelerating square

---
+++ js/sketch.js
@@ -0,0 +1,20 @@
+// Opdracht 5B: Accelerating square
+
+var rectX = 0;
+
+function setup () {
+createCanvas (400, 400);
+}
+
+function draw () {
+background (240);
+
+fill (255,0,0)
+rect (rectX,150, 50,50)
+
+rectX = rectX + 1 
+rectX = rectX * 2
+}
+
+
+
